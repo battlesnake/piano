61f0e2476536c9c81e1749a37537f3e17de4bb7e
Mark K Cowan <mark@battlesnake.co.uk>
2013-11-21 21:04:08 +0000

    Migrated to fluidsynth, although TiMidity++ is still supported via an edit to the script

--- piano.sh
+++ piano.sh
@@ -5,20 +5,43 @@
 # Used to identify controller (MIDI source), in my case an Evolution MK-449C
 CONTROLLER="MK-449C"
 CONTROLLER_PORT=0
-# Used to identify synthesizer (MIDI sink), this script uses TiMidity++ by default
+# Used to identify synthesizer (MIDI sink), e.g. TiMidity, fluidsynth
 SYNTH="TiMidity"
+# Synth program name e.g. timidity, fluidsynth
+SYNTH_PROC="timidity"
 # Port on synthesizer to use (TiMidity has 4 by default, I have Guitar Pro 5 in Wine mapped to ports 2 & 3)
 SYNTH_PORT=0
+# Synth command line
+SYNTH_CMD="timidity -iA -Os --ext=wpvseo -s48000 -B1,8"
 
-# Your sound card may not support this, try 44100 or 48000 (and set BUFFERSIZE to 8 unless it causes choppy sound)
-SAMPLE_RATE=96000
-BUFFERSIZE=10
+[ -z "$FLUIDSYNTH" ] && FLUIDSYNTH=1
+
+if [[ $FLUIDSYNTH ]]
+then
+	SOUNDFONT="/usr/share/sf2/Piano24.sf2"
+	SYNTH="FLUID Synth"
+	SYNTH_PROC="fluidsynth"
+	SYNTH_PORT=0
+	SYNTH_CMD="fluidsynth  --server --no-shell  --audio-driver=jack --audio-bufcount=2 --gain=1 --audio-channels=2 --midi-driver=alsa_seq --sample-rate=48000 --audio-bufsize=64 --connect-jack-outputs  $SOUNDFONT"
+fi
 
 function start {
+	#if [ "$2" == "-k" ]
+	#then
+	#	echo "Restarting pulseaudio"
+	#	pulseaudio -k
+	#	pulseaudio -D
+	#fi
+
 	if ! pidof timidity > /dev/null
 	then
-		echo "Starting timidity synthesizer"
-		timidity -Os -s$SAMPLE_RATE -iA -B2,$BUFFERSIZE & disown
+		echo "Starting $SYNTH synthesizer"
+		CMD="$SYNTH_CMD"
+		echo "\$ $CMD"
+		$CMD &
+		TPID=$!
+		renice -n -5 -p $!
+		disown
 		sleep 1
 	fi
 
@@ -62,12 +85,13 @@ function stop {
 	echo "Clearing ALSA midi connections"
 	aconnect -x
 
-	OLDTIM=`pidof timidity`
-	if [ "$OLDTIM" ]
+	OLDPID=`pidof $SYNTH_PROC`
+	if [ "$OLDPID" ]
 	then
-		echo "Stopping previous Timidity instance"
-		kill $OLDTIM
+		echo "Stopping previous $SYNTH instance"
+		kill -s TERM $OLDPID
 		sleep 0.5
+		kill -s KILL $OLDPID 2>/dev/null
 	fi
 }
 
